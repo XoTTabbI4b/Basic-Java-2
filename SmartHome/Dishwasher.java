f3826e43b4e5b4f92ddc9477826f68ef6e0692c2
Yauheni Bakhar <122569069+XoTTabbI4b@users.noreply.github.com>
2023-05-30 20:47:25 +0100

    Update Dishwasher.java

--- SmartHome/Dishwasher.java
+++ SmartHome/Dishwasher.java
@@ -1,6 +1,19 @@
 package Pro.Lesson1.SmartHome;
 public class Dishwasher extends Appliance {
+    private boolean isCycleComplete; // Состояние завершения текущего цикла мойки
+
     public Dishwasher(String name) {
         super(name);
+        isCycleComplete = false;
+    }
+
+    @Override
+    public void turnOn() {
+        super.turnOn();
+        isCycleComplete = false;
+    }
+
+    public boolean isCycleComplete() {
+        return isCycleComplete;
     }
 }
